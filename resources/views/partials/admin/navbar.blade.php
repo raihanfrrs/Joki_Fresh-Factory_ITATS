c930fb7a0d004de19d195084fe39c27ae9398491
fiqriachmada <achmada1998@gmail.com>
2024-05-14 10:53:15 +0700

    delete icon sampah

--- resources/views/partials/admin/navbar.blade.php
+++ resources/views/partials/admin/navbar.blade.php
@@ -1,36 +1,35 @@
-<nav
-    class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
+<nav class="layout-navbar container-xxl navbar navbar-expand-xl navbar-detached align-items-center bg-navbar-theme"
     id="layout-navbar">
     <div class="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
         <a class="nav-item nav-link px-0 me-xl-4" href="javascript:void(0)">
-        <i class="ti ti-menu-2 ti-sm"></i>
+            <i class="ti ti-menu-2 ti-sm"></i>
         </a>
     </div>
 
     <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
         <!-- Search -->
-        <div class="navbar-nav align-items-center">
+        {{-- <div class="navbar-nav align-items-center">
         <div class="nav-item navbar-search-wrapper mb-0">
             <a class="nav-item nav-link search-toggler d-flex align-items-center px-0" href="javascript:void(0);">
             <i class="ti ti-search ti-md me-2"></i>
             <span class="d-none d-md-inline-block text-muted">Search (Ctrl+/)</span>
             </a>
         </div>
-        </div>
+        </div> --}}
         <!-- /Search -->
 
         <ul class="navbar-nav flex-row align-items-center ms-auto">
 
-        <!-- Style Switcher -->
-        <li class="nav-item me-2 me-xl-0">
-            <a class="nav-link style-switcher-toggle hide-arrow" href="javascript:void(0);">
-            <i class="ti ti-md"></i>
-            </a>
-        </li>
-        <!--/ Style Switcher -->
+            <!-- Style Switcher -->
+            <li class="nav-item me-2 me-xl-0">
+                <a class="nav-link style-switcher-toggle hide-arrow" href="javascript:void(0);">
+                    <i class="ti ti-md"></i>
+                </a>
+            </li>
+            <!--/ Style Switcher -->
 
-        <!-- Quick links  -->
-        {{-- <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
+            <!-- Quick links  -->
+            {{-- <li class="nav-item dropdown-shortcuts navbar-dropdown dropdown me-2 me-xl-0">
             <a
             class="nav-link dropdown-toggle hide-arrow"
             href="javascript:void(0);"
@@ -121,96 +120,92 @@
             </div>
             </div>
         </li> --}}
-        <!-- Quick links -->
+            <!-- Quick links -->
 
-        <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-1">
-            <a
-            class="nav-link dropdown-toggle hide-arrow"
-            href="javascript:void(0);"
-            data-bs-toggle="dropdown"
-            data-bs-auto-close="outside"
-            aria-expanded="false">
-            <i class="ti ti-trash ti-md"></i>
-                <span class="badge bg-danger rounded-pill badge-notifications d-none" id="label-total-account-deleted-count"></span>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end py-0">
-            <li class="dropdown-menu-header border-bottom">
-                <div class="dropdown-header d-flex align-items-center py-3">
-                <h5 class="text-body mb-0 me-auto">Sampah</h5>
-                </div>
-            </li>
-            <div id="data-account-deleted-notification"></div>
-            </ul>
-        </li>
+            {{-- <li class="nav-item dropdown-notifications navbar-dropdown dropdown me-3 me-xl-1">
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown"
+                    data-bs-auto-close="outside" aria-expanded="false">
+                    <i class="ti ti-trash ti-md"></i>
+                    <span class="badge bg-danger rounded-pill badge-notifications d-none"
+                        id="label-total-account-deleted-count"></span>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end py-0">
+                    <li class="dropdown-menu-header border-bottom">
+                        <div class="dropdown-header d-flex align-items-center py-3">
+                            <h5 class="text-body mb-0 me-auto">Sampah</h5>
+                        </div>
+                    </li>
+                    <div id="data-account-deleted-notification"></div>
+                </ul>
+            </li> --}}
 
-        <!-- User -->
-        <li class="nav-item navbar-dropdown dropdown-user dropdown">
-            <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-            <div class="avatar avatar-online">
-                @if (auth()->user()->admin->getFirstMediaUrl('admin'))
-                    <img src="{{ auth()->user()->admin->getFirstMediaUrl('admin') }}" alt="{{ auth()->user()->admin->slug }}" class="h-auto rounded-circle">    
-                @else
-                    {!! generateAvatar(auth()->user()->admin->name) !!}
-                @endif
-            </div>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-end">
-            <li>
-                <a class="dropdown-item" href="{{ route('admin.profile') }}">
-                <div class="d-flex">
-                    <div class="flex-shrink-0 me-3">
+            <!-- User -->
+            <li class="nav-item navbar-dropdown dropdown-user dropdown">
+                <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                     <div class="avatar avatar-online">
                         @if (auth()->user()->admin->getFirstMediaUrl('admin'))
-                            <img src="{{ auth()->user()->admin->getFirstMediaUrl('admin') }}" alt="{{ auth()->user()->admin->slug }}" class="h-auto rounded-circle">    
+                            <img src="{{ auth()->user()->admin->getFirstMediaUrl('admin') }}"
+                                alt="{{ auth()->user()->admin->slug }}" class="h-auto rounded-circle">
                         @else
                             {!! generateAvatar(auth()->user()->admin->name) !!}
                         @endif
                     </div>
-                    </div>
-                    <div class="flex-grow-1">
-                    <span class="fw-semibold d-block">{{ auth()->user()->admin->name }}</span>
-                    <small class="text-muted">Admin</small>
-                    </div>
-                </div>
                 </a>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li>
+                        <a class="dropdown-item" href="{{ route('admin.profile') }}">
+                            <div class="d-flex">
+                                <div class="flex-shrink-0 me-3">
+                                    <div class="avatar avatar-online">
+                                        @if (auth()->user()->admin->getFirstMediaUrl('admin'))
+                                            <img src="{{ auth()->user()->admin->getFirstMediaUrl('admin') }}"
+                                                alt="{{ auth()->user()->admin->slug }}" class="h-auto rounded-circle">
+                                        @else
+                                            {!! generateAvatar(auth()->user()->admin->name) !!}
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="flex-grow-1">
+                                    <span class="fw-semibold d-block">{{ auth()->user()->admin->name }}</span>
+                                    <small class="text-muted">Admin</small>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('admin.profile') }}">
+                            <i class="ti ti-user-check me-2 ti-sm"></i>
+                            <span class="align-middle">My Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('admin.settings.profile') }}">
+                            <i class="ti ti-settings me-2 ti-sm"></i>
+                            <span class="align-middle">Settings</span>
+                        </a>
+                    </li>
+                    <li>
+                        <div class="dropdown-divider"></div>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('logout') }}">
+                            <i class="ti ti-logout me-2 ti-sm"></i>
+                            <span class="align-middle">Logout</span>
+                        </a>
+                    </li>
+                </ul>
             </li>
-            <li>
-                <div class="dropdown-divider"></div>
-            </li>
-            <li>
-                <a class="dropdown-item" href="{{ route('admin.profile') }}">
-                <i class="ti ti-user-check me-2 ti-sm"></i>
-                <span class="align-middle">My Profile</span>
-                </a>
-            </li>
-            <li>
-                <a class="dropdown-item" href="{{ route('admin.settings.profile') }}">
-                <i class="ti ti-settings me-2 ti-sm"></i>
-                <span class="align-middle">Settings</span>
-                </a>
-            </li>
-            <li>
-                <div class="dropdown-divider"></div>
-            </li>
-            <li>
-                <a class="dropdown-item" href="{{ route('logout') }}">
-                <i class="ti ti-logout me-2 ti-sm"></i>
-                <span class="align-middle">Logout</span>
-                </a>
-            </li>
-            </ul>
-        </li>
-        <!--/ User -->
+            <!--/ User -->
         </ul>
     </div>
 
     <!-- Search Small Screens -->
     <div class="navbar-search-wrapper search-input-wrapper d-none">
-        <input
-        type="text"
-        class="form-control search-input container-xxl border-0"
-        placeholder="Search..."
-        aria-label="Search..." />
+        <input type="text" class="form-control search-input container-xxl border-0" placeholder="Search..."
+            aria-label="Search..." />
         <i class="ti ti-x ti-sm search-toggler cursor-pointer"></i>
     </div>
 </nav>
